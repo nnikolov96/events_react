4374928855b88296701fb70e5aa9faeea5cb1052
Nikola <nikolanenchevnikolov@gmail.com>
2020-11-03 15:58:14 +0200

    links in header for home, sign in and sign up

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 function AppHeader() {
   const currentUser = localStorage.getItem('user')
@@ -19,15 +20,28 @@ function AppHeader() {
 
   return (
     <div>
-      {currentUser &&
-        <div style={{ float: "right" }}>
-          {JSON.parse(currentUser).uid}
-          <a href="#" onClick={handleSignOut} >Sign out</a>
-        </div>
-      }
-      <h1 className="logo">Eventlite</h1>
+      <div style={{ float: "right" }}>
+        {currentUser ? (
+          <>
+            {JSON.parse(currentUser).uid}
+            <a href="#" onClick={handleSignOut}>
+              Sign out
+            </a>
+          </>
+        ) : (
+          <>
+            <Link to="/signup">Signup</Link>
+            <Link to="/login" style={{ padding: "10px" }}>
+              Login
+            </Link>
+          </>
+        )}
+      </div>
+      <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+        <h1 className="logo">Eventlite</h1>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default AppHeader;
